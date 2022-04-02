8f9915e7510a3939057ec035ffbefb9d106d0e69
Adarsh Verma <55326493+Tryingtroyl@users.noreply.github.com>
2022-04-02 23:00:50 +0530

    Sends data to the booking api

--- js/booking.js
+++ js/booking.js
@@ -1,14 +1,28 @@
-jQuery("https://p4bkuyvb1h.execute-api.ap-south-1.amazonaws.com/dev/booking").submit(function(e){
-    //The following stops the form from redirecting
-    e.preventDefault();
-    jQuery("https://p4bkuyvb1h.execute-api.ap-south-1.amazonaws.com/dev/booking").ajaxSubmit({
-        type: 'POST',
-        url: jQuery('https://p4bkuyvb1h.execute-api.ap-south-1.amazonaws.com/dev/booking').attr('action'),
-        data: jQuery('https://p4bkuyvb1h.execute-api.ap-south-1.amazonaws.com/dev/booking').serialize(),
-        success: function (data) {
-            //The data variable will contain the response data
-            //if it's successful, you can no redirect wherever you want
-            window.location.href = "booking.html";
+console.log("FAHFHAJHFAJFH");
+    function logSubmit(event) {
+        var form = document.querySelector('form');
+        var formData = new FormData(form);
+    console.log("HERE");
+    var object = {};
+    formData.forEach(function(value, key){
+        object[key] = value;
+        });
+    var json = JSON.stringify(object);
+    console.log(json);
+    $.ajax({
+        type: "POST",
+        url: "https://p4bkuyvb1h.execute-api.ap-south-1.amazonaws.com/dev/booking",
+        data: json,
+        contentType: "application/json",
+        success: function (result) {
+          console.log(result);
+        },
+        error: function (result, status) {
+          console.log(result);
         }
-    });
-});
+      });
+    event.preventDefault();
+    }
+
+    var form = document.querySelector('form');
+    form.addEventListener('submit', logSubmit);
